ae191f28f7ac1d1b58574873476ca9224517d051
Shawn Sandy <creative@shawnsandy.design>
2021-03-27 16:09:11 -0400

    Edit: add logos and tokens

--- system/_data/style.js
+++ system/_data/style.js
@@ -1,5 +1,5 @@
 const COMPONENTS = require('../style-guide/data/components.json')
-const FONTS = require('../style-guide/data/components.json')
+const FONTS = require('../style-guide/data/fonts.json')
 const ELEMENTS = require('../style-guide/data/components.json')
 
 module.exports = {
